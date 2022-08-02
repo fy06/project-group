02edf3fef2c6a94290162f121b13549a1915b16f
bobacakee <s10242888@connect.np.edu.sg>
2022-08-02 11:25:24 +0800

    1st

--- profit_loss.py
+++ profit_loss.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import re
 file_path = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
 summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def profit_loss_function(forex):
