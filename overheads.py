464cf20065ab4c0f2d8c9a3b888653ea11b589b6
fy06 <aifangyi02@gmail.com>
2022-08-02 12:29:42 +0800

    1st commit

--- overheads.py
+++ overheads.py
@@ -4,3 +4,10 @@ summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def overhead_function(forex):
     with file_path.open(mode="r",encoding= 'UTF-8') as file:
         next(file)
+        data_list = []
+        highest_value = 0
+        highest_value_index = 0
+        for line in file:
+            line_data = line.strip().split('\n')[0].split(",")
+            data_list.append(line_data)
+            
