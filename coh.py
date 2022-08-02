43eb5b4c60fec3ba3a87ec3e3974d7076b4e8d71
tiraaa <atiraa333@gmail.com>
2022-08-02 21:46:13 +0800

    6th

--- coh.py
+++ coh.py
@@ -5,14 +5,14 @@ summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def coh_function(forex):
     with file_path.open(mode="r",encoding='UTF-8',newline='') as file:
         next(file)
-        prev_day = 8
+        prev_day = 0
         diff = 0 
         for line in file.readlines():
             line = re.findall(pattern=r'[0-9][0-9]+',string=line)
             diff = float(line[1]) - prev_day
-            prev_day = float(line[1])
+            prev_day = float(line[4])
             if diff<0:
                 diff = round(number)(abs(diff)*forex,1)
-                with summary_path.open(mode="w",encoding='UTF-8',newline='') as file:
+                with summary_path.open(mode="a",encoding='UTF-8',newline='') as file:
                     file.writtenlines("[CASH DEFECIT]"+"DAY: "+str(round(float(line[0],1))))       
     file.close() 
