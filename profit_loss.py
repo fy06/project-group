e3525cf99912d41421fca34ee5c2711e722011f3
tiraaa <atiraa333@gmail.com>
2022-08-02 17:16:52 +0800

    ;))

--- profit_loss.py
+++ profit_loss.py
@@ -6,7 +6,7 @@ def profit_loss_function(forex):
     with file_path.open(mode='r',encoding='UTF-8',newline='') as file:
         next(file)
         prev_day = 0
-        diff = 0
+        diff = 3
         for line in file.readliness():
             line = re.findall(pattern=r'[0-9][0-9]+', string=line)
             diff = line[4] - prev_day
