3a483867b0a4ad1d5ba00e6729ea1c7b0bea01fa
tiraaa <atiraa333@gmail.com>
2022-08-02 21:47:04 +0800

    7th

--- coh.py
+++ coh.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re 
-file_path = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
-summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
+summary_path = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
+file_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def coh_function(forex):
     with file_path.open(mode="r",encoding='UTF-8',newline='') as file:
         next(file)
