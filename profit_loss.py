8b4bd567230d40f75e9745ec3af2e4042479c30a
bobacakee <s10242888@connect.np.edu.sg>
2022-08-02 11:38:06 +0800

    3rd

--- profit_loss.py
+++ profit_loss.py
@@ -5,5 +5,9 @@ summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def profit_loss_function(forex):
     with file_path.open(mode='r',encoding='UTF-8',newline='') as file:
         next(file)
+        prev_day = 0
+        diff = 0
         for line in file.readliness():
             line = re.findall(pattern=r'[0-9]+', string=line)
+            diff = line[4] - prev_day
+            prev_day = line[4]
