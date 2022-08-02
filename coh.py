f711f59bcde5630c3b2eb863c8ff5bdbeb59048c
tiraaa <atiraa333@gmail.com>
2022-08-02 21:53:49 +0800

    13th

--- coh.py
+++ coh.py
@@ -12,7 +12,7 @@ def coh_function(forex):
             diff = float(line[1]) - prev_day
             prev_day = float(line[4])
             if diff<0:
-                diff = round(number)(abs(diff)*forex,1)
+                diff = round(number)(abs(diff)*forex)
                 with summary_path.open(mode="a",encoding='UTF-8',newline='') as file:
                     file.writtenlines("[CASH DEFECIT]"+"DAY: "+str(round(float(line[0],1))+". AMOUNT: SGD"+str(diff)+"\n"))       
     file.close() 
