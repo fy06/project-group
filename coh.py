f3e92bb30634404e65508366d1321479c8d53571
tiraaa <atiraa333@gmail.com>
2022-08-03 22:19:21 +0800

    15th

--- coh.py
+++ coh.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re 
-summary_path = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
-file_path = Path.cwd()/"csv_reports"/"summary_report.txt"
+file_path = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
+summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 def coh_function(forex):
     with file_path.open(mode="r",encoding='UTF-8',newline='') as file:
         next(file)
@@ -10,7 +10,9 @@ def coh_function(forex):
         for line in file.readlines():
             line = re.findall(pattern=r'[0-9][0-9]+',string=line)
             diff = float(line[1]) - prev_day
-            prev_day = float(line[4])
+            prev_day = float(line[1])
             if diff<0:
                 diff = round(abs(diff)*forex,1)
-                with summary_path.open(mode="w",encoding='UTF-8',newline='') as file:
+                with summary_path.open(mode="a",encoding='UTF-8',newline='') as file:
+                    file.writelines("[CASH DEFECIT]"+"DAY: "+str(round(float(line[0],1))+",AMOUNT: SGD"+str(diff)+"\n"))
+    file.close()
