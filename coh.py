ffb62472a236c1bda3b75767d2e87fbec051628a
fy06 <aifangyi02@gmail.com>
2022-08-04 18:18:42 +0800

    3rd commit

--- coh.py
+++ coh.py
@@ -7,12 +7,20 @@ def coh_function(forex):
         next(file)
         prev_day = 0
         diff = 0 
+        data_list = []
         for line in file.readlines():
             line = re.findall(pattern=r'[0-9][0-9]+',string=line)
-            diff = float(line[1]) - prev_day
-            prev_day = float(line[1])
+            data_list.append(line)
+    with summary_path.open(mode="a",encoding='UTF-8',newline='') as file:
+        deficit_presence = False
+        for line in range(1,len(data_list)):
+            prev_day = float(data_list[line-1][1])
+            diff = float(data_list[line][1]) - prev_day
             if diff<0:
                 diff = round(abs(diff)*forex,1)
-                with summary_path.open(mode="a",encoding='UTF-8',newline='') as file:
-                    file.writelines("[CASH DEFECIT]"+"DAY: "+str(round(float(line[0],1))+",AMOUNT: SGD"+str(diff)+"\n"))
+                file.writelines("[CASH DEFECIT]"+"DAY: "+str(float(data_list[line][0]))+",AMOUNT: SGD"+str(diff)+"\n"))
+                deficit_presence = True
+            else:
+                if diff > 0 and line == (len(data_list)-1) and deficit_presence == False:
+                    file.writelines("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
     file.close()
